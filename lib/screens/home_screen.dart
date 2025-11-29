1695f4d80fe7a761713977a533f7f400424f8709
dwiilhammaulana <1123150008@global.ac.id>
2025-11-29 22:36:08 +0700

    coba animasi dilokal

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          'Selamat datang di aplikasi!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
