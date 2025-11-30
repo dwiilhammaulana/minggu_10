3fe94e6aedd52f1c69a99c1ca528e623a1088b4f
dwiilhammaulana <1123150008@global.ac.id>
2025-11-30 20:47:33 +0700

    menambahkan models folder

---
+++ lib/models/note_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Note extends StatelessWidget {
+  const Note({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
